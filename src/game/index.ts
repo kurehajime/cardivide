87dda8d2d8b73034070c8303e4afd3e65d638036
kurehajime <xiidec@gmail.com>
2026-08-15 22:45:27 +0900

    カードリスト

--- src/game/index.ts
+++ src/game/index.ts
@@ -1,5 +1,6 @@
 export { GameAI } from './GameAI'
 export { GameManager } from './GameManager'
+export { CARD_BY_ID, CARD_LIST, CREATURE_CARDS, FORMATION_CARDS, SPELL_CARDS } from './cards'
 export type {
   AbilityText,
   Board,
